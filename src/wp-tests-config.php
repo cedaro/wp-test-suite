62011e2b5388f2b7412c55157fedff18e1ce7426
Brady Vercher <brady@blazersix.com>
2019-03-22 07:24:49 -0700

    Allow the database credentials to be read from environment variables.

--- src/wp-tests-config.php
+++ src/wp-tests-config.php
@@ -25,10 +25,10 @@ define( 'WP_DEBUG', true );
 // These tests will DROP ALL TABLES in the database with the prefix named below.
 // DO NOT use a production database or one that is shared with something else.
 
-define( 'DB_NAME',     'wordpress_test' );
-define( 'DB_USER',     'root' );
-define( 'DB_PASSWORD', '' );
-define( 'DB_HOST',     'localhost' );
+define( 'DB_NAME',     getenv( 'WP_TESTS_DB_NAME' ) ?: 'wordpress_test' );
+define( 'DB_USER',     getenv( 'WP_TESTS_DB_USER' ) ?: 'root' );
+define( 'DB_PASSWORD', getenv( 'WP_TESTS_DB_PASSWORD' ) ?: '' );
+define( 'DB_HOST',     getenv( 'WP_TESTS_DB_HOST' ) ?: 'localhost' );
 define( 'DB_CHARSET',  'utf8mb4' );
 define( 'DB_COLLATE',  '' );
 
